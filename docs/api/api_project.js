af4ff60171981832af01008bae21dae31bbe1f07
Unknown <russleyshaw@gmail.com>
2017-02-14 13:55:06 -0600

    Added docs

---
+++ docs/api/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "colisee-api",
+  "version": "1.0.0",
+  "description": "Micro-service responsible for scheduling random games for SIG-Game Colisee.",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-02-14T18:31:15.763Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.5"
+  }
+});
