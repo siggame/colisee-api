e01900cc818b11ac9f8a5d24352cda4bbfe4adf1
user404d <conduffql@gmail.com>
2017-03-21 17:04:48 -0500

    Fix joinTeamData to return Team object

--- src/teams.ts
+++ src/teams.ts
@@ -2,11 +2,11 @@ import { User } from "./users";
 import * as conn from "./conn";
 
 export interface Team {
-    id: number;
-    name: string;
-    stub: string;
-    description: string;
-    captian_id: number;
+  id: number;
+  name: string;
+  stub: string;
+  description: string;
+  captain_id: number;
 }
 
 export interface GetTeamsOptions {
@@ -18,13 +18,13 @@ export interface CreateTeamOptions {
 }
 
 export interface UpdateTeamOptions {
-    
+
 }
 
 export function getTeams(options: GetTeamsOptions): Promise<Team[]> {
-    return new Promise<Team[]>((resolve, reject)=>{
-        reject(new Error("NOT IMPLEMENTED"));
-    });
+  return new Promise<Team[]>((resolve, reject) => {
+    reject(new Error("NOT IMPLEMENTED"));
+  });
 }
 
 /**
@@ -32,22 +32,22 @@ export function getTeams(options: GetTeamsOptions): Promise<Team[]> {
  * @returns Promise of Team
  */
 export function getTeam(id: number): Promise<Team> {
-    return new Promise<Team>((resolve, reject)=>{
-        const gl = 0;
-        const db = 1;
+  return new Promise<Team>((resolve, reject) => {
+    const gl = 0;
+    const db = 1;
 
-        const promises = [
-            conn.db("team").where("id", id).then(),
-            conn.gitlab({url: `projects/${id}`})
-        ];
-        Promise.all(promises)
-            .then(results=>{
-         //       if(id)
-            })   //return statement here, but inside this if statement? Does this if even go here? All this and more in the next season of ask Russley
-         //   .then(resolve)
-            .catch(reject);
+    const promises = [
+      conn.db("team").where("id", id).then(),
+      conn.gitlab({ url: `projects/${id}` })
+    ];
+    Promise.all(promises)
+      .then(results => {
+        //       if(id)
+      })   //return statement here, but inside this if statement? Does this if even go here? All this and more in the next season of ask Russley
+      //   .then(resolve)
+      .catch(reject);
 
-    });
+  });
 }
 
 /**
@@ -55,9 +55,9 @@ export function getTeam(id: number): Promise<Team> {
  * @returns Promise of created User
  */
 export function createTeam(options: CreateTeamOptions): Promise<Team> {
-    return new Promise<Team>((resolve, reject)=>{
-        reject(new Error("NOT IMPLEMENTED"));
-    });
+  return new Promise<Team>((resolve, reject) => {
+    reject(new Error("NOT IMPLEMENTED"));
+  });
 }
 
 /**
@@ -66,19 +66,19 @@ export function createTeam(options: CreateTeamOptions): Promise<Team> {
  * @returns Promise of updated Team
  */
 export function updateTeam(id: number, options: UpdateTeamOptions): Promise<Team> {
-    return new Promise<Team>((resolve, reject)=>{
-        reject(new Error("NOT IMPLEMENTED"));
-    });
+  return new Promise<Team>((resolve, reject) => {
+    reject(new Error("NOT IMPLEMENTED"));
+  });
 }
 
 /**
  * @param id Team id
  * @returns Promise if Team was deleted
  */
-export function deleteTeam(id: number): Promise<void>{
-    return new Promise<void>((resolve, reject)=>{
-        reject(new Error("NOT IMPLEMENTED"));
-    });
+export function deleteTeam(id: number): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    reject(new Error("NOT IMPLEMENTED"));
+  });
 }
 
 /**
@@ -86,18 +86,20 @@ export function deleteTeam(id: number): Promise<void>{
  * @returns Promise of Team members
  */
 export function getTeamMembers(id: number): Promise<User[]> {
-    return new Promise<User[]>((resolve, reject)=>{
-        reject(new Error("NOT IMPLEMENTED"));
-    });
+  return new Promise<User[]>((resolve, reject) => {
+    reject(new Error("NOT IMPLEMENTED"));
+  });
 }
 
 export function joinTeamData(gitlabData: any, dbData: any): Team {
-    if(gitlabData.id !== dbData.id) {
-        throw new Error(`Cannot join team datas with different ids: ${gitlabData.id} and ${dbData.id}`);
-    }
-    return {
-        id: gitlabData.id,
-        name: gitlabData.name,
-
-    }
+  if (gitlabData.id !== dbData.id) {
+    throw new Error(`Cannot join team datas with different ids: ${gitlabData.id} and ${dbData.id}`);
+  }
+  return {
+    id: gitlabData.id,
+    name: gitlabData.name,
+    stub: dbData.stub,
+    description: gitlabData.description,
+    captain_id: dbData.captain_id
+  }
 }
